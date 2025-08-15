0535d0782ba0f8249a5649b267bbb8c33d6120e7
Vedant <vedantlamba18@gmail.com>
2025-08-15 18:39:06 +0530

    trying safari width and size on mobiles

--- components/Landing/components/Hero/landing-hero.tsx
+++ components/Landing/components/Hero/landing-hero.tsx
@@ -59,7 +59,7 @@ function LandingHero() {
         <div className="relative flex justify-center items-center px-4 md:px-0">
           <Safari
             url="magicui.design"
-            className="md:size-1/2 size-fit"
+            className="md:size-1/2 size-full"
             videoSrc="https://videos.pexels.com/video-files/3115209/3115209-uhd_2560_1440_25fps.mp4"
           />
         </div>
